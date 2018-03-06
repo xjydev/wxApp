4d0786a6bd8a3afc3e27ce74e79c20fcf279dc77
XiaoDev <jingyuan.xiao@haiwaifenqi.com>
2018-03-06 16:36:03 +0800

    init

---
+++ WXInfo/pages/index/index.js
@@ -0,0 +1,125 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    
+    userInfo: {},
+    hasUserInfo: false,
+    hasNetworkType: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    systemInfo: {},
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+      // var that = this
+    }
+    this.getSystemInfo();
+    // wx.getSystemInfo({
+    //   success: function (res) {
+    //     that.setData({
+    //       systemInfo: res,
+    //     })
+    //     that.update()
+    //   }
+    // })
+    this.getNetworkType();
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  getNetworkType: function () {
+    var that = this
+    wx.getNetworkType({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          hasNetworkType: true,
+          networkType: res.subtype || res.networkType
+        })
+        // that.update()
+      }
+    })
+  },
+  getSystemInfo: function () {
+    var that = this
+    wx.getSystemInfo({
+      success: function (res) {
+          
+        that.setData({
+          systemInfo: res,
+        })
+        // that.update() 
+        wx.showToast({
+          title: "刷新成功",
+          duration: 400
+        })
+      }
+    })
+  },
+  onShareAppMessage: function () {
+    // shareData: {
+    //   title: userInfo.nickName + '的手机',
+    //   desc: '自定义分享描述',
+    //   path: '/page/API/pages/share/share'
+    // }
+    
+    return {
+      title: "手机设备信息",
+      desc:"信息来自微信小程序“手机设备信息”",
+      path:"/pages/index/index",
+      success: function (res) {
+        wx.showToast({
+          title: "转发成功",
+          duration: 400
+        })
+        // 转发成功
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: "转发失败",
+          icon:"none",
+          duration: 400
+        })
+        // 转发失败
+      }
+    }
+   
+  }
+})
